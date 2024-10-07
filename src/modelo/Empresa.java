c218f3d35ef61c2d5c0abb1c35062cb69bee0299
albertGil21 <alberto.gil@unmsm.edu.pe>
2024-10-07 10:36:01 -0500

    Agregando clases Empresa y ReclutadorContacto

---
+++ src/modelo/Empresa.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Empresa {
+    private String nombre;
+    private String RUC;
+    private String rubroEmpresarial;
+    private String correoInstitucional;
+    private String contraseña;
+    ReclutadorContacto reclutadorContacto;
+    public Empresa(){};
+}
---
+++ src/modelo/ReclutadorContacto.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public class ReclutadorContacto {
+    private String nombres;
+    private String apellidos;
+    private String DNI;
+    private String puesto;
+}
